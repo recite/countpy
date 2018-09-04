03c53bbcd8352e56fed0cda75290d9240bca8fc9
khanh <khanhtd@gmail.com>
2018-09-04 16:20:42 +0700

    Debug and fix Repository model

--- app/models.py
+++ app/models.py
@@ -9,12 +9,19 @@ Contains all the model objects and their schemas.
 import json
 from datetime import datetime
 from typing import Dict, Any
-
 from . import db
 
 __all__ = ['Repository']
 
-KEYSEP = ':'
+_KEYSEP = ':'
+
+
+def _is_notset(variable):
+    return variable is None or type(variable) is type(Ellipsis)
+
+
+def _is_set(variable):
+    return not _is_notset(variable)
 
 
 class HashType:
@@ -23,11 +30,13 @@ class HashType:
     _json_fields = ()
     _date_fields = ('updated',)
 
+    updated = ...  # type: datetime
+
     @classmethod
     def genkey(cls, name):
         prefix = cls._prefix
-        if prefix and not prefix.endswith(KEYSEP):
-            prefix = '{}{}'.format(prefix, KEYSEP)
+        if prefix and not prefix.endswith(_KEYSEP):
+            prefix = '{}{}'.format(prefix, _KEYSEP)
         if name.startswith(prefix):
             return name
         return '{}{}'.format(prefix, name)
@@ -51,8 +60,10 @@ class HashType:
     @classmethod
     def mset(cls, name, mapping):
         assert len(mapping) > 0, 'Nothing to set.'
-        mapping['updated'] = datetime.utcnow()
-        return db.hmset(cls.genkey(name), {k: cls.store_val(v, k) for k, v in mapping.items()})
+        now = datetime.utcnow()
+        mapping['updated'] = now
+        db.hmset(cls.genkey(name), {k: cls.store_val(v, k) for k, v in mapping.items()})
+        return now
 
     @classmethod
     def get(cls, name, field):
@@ -72,15 +83,16 @@ class HashType:
 
     @classmethod
     def store_val(cls, value, field):
-        if field in cls._text_fields:
-            return str(value)
-        elif field in cls._json_fields:
-            return json.dumps(value)
-        elif field in cls._date_fields:
-            if isinstance(value, datetime):
-                return str(value.timestamp())
-            elif isinstance(value, (float, int, str)):
-                return value
+        if _is_set(value):
+            if field in cls._text_fields:
+                return str(value)
+            elif field in cls._json_fields:
+                return json.dumps(value)
+            elif field in cls._date_fields:
+                if isinstance(value, datetime):
+                    return str(value.timestamp())
+                elif isinstance(value, (float, int, str)):
+                    return value
         raise NotImplementedError('Unsupported field "%s" with value "%s"' % (field, value))
 
     @classmethod
@@ -88,44 +100,57 @@ class HashType:
         if field in cls._text_fields:
             return value
         elif field in cls._json_fields:
-            return json.loads(value)
+            if _is_set(value):
+                return json.loads(value)
+            return None
         elif field in cls._date_fields:
-            return datetime.utcfromtimestamp(float(value))
+            if _is_set(value):
+                return datetime.utcfromtimestamp(float(value))
+            return None
         raise NotImplementedError('Unsupported field "%s" with value "%s"' % (field, value))
 
     @classmethod
     def all_fields(cls):
         return cls._text_fields + cls._json_fields + cls._date_fields
 
-    def __new__(cls, name, **kwargs):
-        if kwargs:
+    def __new__(cls, name, **attrs):
+        if attrs:
             if cls.exists(name):
                 raise IOError('Already existed, only accepts "name" for init %s.' % cls)
-            assert set(kwargs.keys()) <= set(cls.all_fields())
+            assert set(attrs.keys()) <= set(cls.all_fields())
         return object.__new__(cls)
 
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, **attrs):
         self.name = name
-        self.__cache(**kwargs)
+        self.__cache(**attrs)
         self.__load()
 
     def __str__(self):
         return '%s(name=%s)' % (self.__class__.__name__, self.name)
 
-    def __load(self):
-        for field, value in self.getall(self.name):
-            if not hasattr(self, field):
-                setattr(self, field, value)
+    def __repr__(self):
+        return str(self)
 
-    def __cache(self, **kwargs):
-        if kwargs:
-            for field, value in kwargs.items():
+    def __load(self):
+        for field, value in self.getall(self.name).items():
+            if hasattr(self, field) and _is_set(getattr(self, field)):
+                continue
+            setattr(self, field, value)
+
+    def __cache(self, **attrs):
+        if attrs:
+            for field, value in attrs.items():
                 setattr(self, field, value)
 
     def commit(self):
-        mapping = {field: getattr(self, field) for field in self.all_fields() if hasattr(self, field)}
+        mapping = {}
+        for field in self.all_fields():
+            if hasattr(self, field):
+                value = getattr(self, field)
+                if _is_set(value):
+                    mapping[field] = value
         if mapping:
-            self.mset(self.name, mapping)
+            self.updated = self.mset(self.name, mapping)
 
     def is_existed(self):
         return self.exists(self.name)
@@ -140,7 +165,7 @@ class Repository(HashType):
     pyfiles = ...  # type: Dict[Any, Any]
 
     def add_pyfile(self, path, content):
-        if self.pyfiles is None:
+        if _is_notset(self.pyfiles):
             self.pyfiles = {}
         self.pyfiles[path] = content
 

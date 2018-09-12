fe3197c6ed43316b1e2e1a3de2c34e0db2db75b9
khanh <khanhtd@gmail.com>
2018-09-12 09:46:42 +0700

    Finalize DB models. Build RepoFiles struct and content parsing.

--- app/models.py
+++ app/models.py
@@ -6,14 +6,19 @@ app.models
 Contains all the model objects and their schemas.
 """
 
+import os
+import re
 import json
 from datetime import datetime
-from typing import Dict, Any
+from typing import Set, List, Dict, DefaultDict
+from collections import namedtuple, defaultdict
 from . import db
 
 __all__ = ['Repository']
 
 _KEYSEP = ':'
+
+RepoFile = namedtuple('RepoFile', 'path content')
 
 
 def _is_notset(variable):
@@ -26,9 +31,13 @@ def _is_set(variable):
 
 class HashType:
     _prefix = ''
+
+    _num_fields = ()
     _text_fields = ()
     _json_fields = ()
     _date_fields = ('updated',)
+
+    _defaults = {}
 
     updated = ...  # type: datetime
 
@@ -84,41 +93,43 @@ class HashType:
     @classmethod
     def store_val(cls, value, field):
         if _is_set(value):
-            if field in cls._text_fields:
+            if field in cls._text_fields or field in cls._num_fields:
                 return str(value)
             elif field in cls._json_fields:
-                return json.dumps(value)
+                return json.dumps(list(value) if isinstance(value, set) else value)
             elif field in cls._date_fields:
                 if isinstance(value, datetime):
                     return str(value.timestamp())
                 elif isinstance(value, (float, int, str)):
-                    return value
+                    return str(value)
         raise NotImplementedError('Unsupported field "%s" with value "%s"' % (field, value))
 
     @classmethod
     def use_val(cls, value, field):
         if field in cls._text_fields:
             return value
+        elif field in cls._num_fields:
+            return int(value) if _is_set(value) else None
         elif field in cls._json_fields:
-            if _is_set(value):
-                return json.loads(value)
-            return None
+            return json.loads(value) if _is_set(value) else None
         elif field in cls._date_fields:
-            if _is_set(value):
-                return datetime.utcfromtimestamp(float(value))
-            return None
+            return datetime.utcfromtimestamp(float(value)) if _is_set(value) else None
         raise NotImplementedError('Unsupported field "%s" with value "%s"' % (field, value))
 
     @classmethod
     def all_fields(cls):
-        return cls._text_fields + cls._json_fields + cls._date_fields
+        return cls._num_fields + cls._text_fields + cls._json_fields + cls._date_fields
 
     def __new__(cls, name, **attrs):
         if attrs:
             if cls.exists(name):
                 raise IOError('Already existed, only accepts "name" for init %s.' % cls)
             assert set(attrs.keys()) <= set(cls.all_fields())
-        return object.__new__(cls)
+        instance = object.__new__(cls)
+        instance._changes = set()
+        if not cls.exists(name):
+            instance.set_change('name')
+        return instance
 
     def __init__(self, name, **attrs):
         self.name = name
@@ -131,20 +142,45 @@ class HashType:
     def __repr__(self):
         return str(self)
 
-    def __load(self):
-        for field, value in self.getall(self.name).items():
-            if hasattr(self, field) and _is_set(getattr(self, field)):
-                continue
-            setattr(self, field, value)
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return other.storekey == self.storekey
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self.storekey)
 
     def __cache(self, **attrs):
         if attrs:
             for field, value in attrs.items():
                 setattr(self, field, value)
+                self.set_change(field)
 
-    def commit(self):
+    def __load(self):
+        for field, value in self.getall(self.name).items():
+            if self.is_changed(field):  # object is initialized with attributes
+                continue
+            setattr(self, field, value if _is_set(value) else self._defaults.get(field))
+
+    @property
+    def storekey(self):
+        return self.genkey(self.name)
+
+    def has_changes(self):
+        return len(self._changes) > 0
+
+    def is_changed(self, field):
+        return field in self._changes
+
+    def set_change(self, *fields):
+        self._changes.update(fields)
+
+    def __commit(self, fields):
         mapping = {}
-        for field in self.all_fields():
+        for field in fields:
             if hasattr(self, field):
                 value = getattr(self, field)
                 if _is_set(value):
@@ -152,22 +188,299 @@ class HashType:
         if mapping:
             self.updated = self.mset(self.name, mapping)
 
+    def commit_changes(self):
+        if self.has_changes():
+            self.__commit(fields=self._changes)
+
+    def commit_all(self):
+        self.__commit(fields=self.all_fields())
+
     def is_existed(self):
         return self.exists(self.name)
+
+
+class RepoFiles:
+    _pyfile = 'pyfile'
+    _reqfile = 'reqfile'
+
+    _ftypes = (_pyfile, _reqfile)
+
+    _find_lines = {
+        _pyfile: re.compile(r'(?:^|\n)\s*((?:import|from).+?(?<!\\)(?=\n|$))', flags=re.DOTALL).findall,
+        _reqfile: re.compile(r'(?:^|\n)\s*(?!#)\s*([^\s].+?)(?<!\\)(?= #|\n|$)', flags=re.DOTALL).findall
+    }
+
+    _find_packages = {
+        _pyfile: re.compile(
+            r'(?:^|\n)\s*(?:from|import) +(.+?)(?= +import|(?<!\\)\n|$)', flags=re.DOTALL).findall,
+
+        _reqfile: re.compile(
+            r'(?:^|\n)\s*(\w[\w-]+)(?:\s*\[[\w\s,-]+\])?'
+            r'(\s*[!~<=>]{1,2}\s*\d+(?:\.\d+)*(?:\s*,\s*[!~<=>]{1,2}\s*\d+(?:\.\d+)*)*)?', flags=re.DOTALL).findall
+    }
+
+    @staticmethod
+    def is_pyfile(path):
+        fname = os.path.basename(path)
+        return fname.lower().endswith('.py')
+
+    @staticmethod
+    def is_reqfile(path):
+        fname = os.path.basename(path)
+        return fname.lower() == 'requirements.txt'
+
+    @staticmethod
+    def pkgname_from_path(path):
+        dir_, base = os.path.dirname(path), os.path.basename(path)
+        if dir_ in ('', '/'):
+            return base.rsplit('.', maxsplit=1)[0] if RepoFiles.is_pyfile(base) else base
+        else:
+            return RepoFiles.pkgname_from_path(dir_)
+
+    @classmethod
+    def get_ftype(cls, path):
+        if cls.is_pyfile(path):
+            return cls._pyfile
+        elif cls.is_reqfile(path):
+            return cls._reqfile
+        return None
+
+    @classmethod
+    def retract_content(cls, content, ftype):
+        assert ftype in cls._ftypes, 'Unknown ftype "%s"' % ftype
+        finder = cls._find_lines.get(ftype)
+        return '\n'.join(finder(content))
+
+    @classmethod
+    def parse_pyfile(cls, content):
+        finder = cls._find_packages[cls._pyfile]
+        ret = set()
+        for found in finder(content):
+            if not found.strip().startswith('.'):
+                for part in found.split(','):
+                    pkgname = part.rsplit('as', maxsplit=1)[0].split('.', maxsplit=1)[0]
+                    ret.add(pkgname.strip())
+        return ret
+
+    @classmethod
+    def parse_reqfile(cls, content):
+        finder = cls._find_packages[cls._reqfile]
+        return {pkgname.lower().strip(): version.strip() for pkgname, version in finder(content)}
+
+    def local_packages(self):
+        return set(self.pkgname_from_path(path) for path, _ in self.pyfiles)
+
+    @classmethod
+    def __struct_init(cls, other_struct):
+        if _is_notset(other_struct):
+            return {cls._pyfile: dict(), cls._reqfile: dict()}
+        elif isinstance(other_struct, dict):
+            if set(other_struct.keys()) == set(cls._ftypes):
+                if all(isinstance(other_struct[i], dict) for i in cls._ftypes):
+                    return other_struct
+        raise TypeError('Unsupported struct: %r' % other_struct)
+
+    def as_dict(self):
+        return self._files
+
+    def __init__(self, files=None):
+        self._files = self.__struct_init(files)
+
+    def __iter__(self):
+        for file in self.pyfiles:
+            yield file
+        if self.reqfile:
+            yield self.reqfile
+
+    def __getitem__(self, path):
+        ftype = self.get_ftype(path)
+        if ftype is not None:
+            return self._files[ftype][path]
+        raise KeyError(path)
+
+    def __setitem__(self, path, content):
+        ftype = self.get_ftype(path)
+        if ftype is not None:
+            content = self.retract_content(content, ftype)
+            if ftype == self._pyfile:
+                self._files[ftype][path] = content
+            elif ftype == self._reqfile:
+                self._files[ftype] = {path: content}
+        raise KeyError(path)
+
+    def __contains__(self, path):
+        ftype = self.get_ftype(path)
+        if ftype is not None:
+            return path in self._files[ftype]
+        return False
+
+    @property
+    def pyfiles(self):
+        return [RepoFile(*i) for i in self._files[self._pyfile].items()]
+
+    @property
+    def reqfile(self):
+        try:
+            return RepoFile(*next(iter(self._files[self._reqfile].items())))
+        except StopIteration:
+            return None
+
+
+class Package(HashType):
+    _prefix = 'pkg'
+
+    _num_fields = ('num_pyfiles', 'num_reqfiles', 'num_repos')
+    _json_fields = ('pyfiles', 'reqfiles', 'repos')
+
+    _defaults = {
+        'num_pyfiles': 0,
+        'num_reqfiles': 0,
+        'num_repos': 0,
+        'repos': set(),
+        'reqfiles': dict(),
+        'pyfiles': defaultdict(set)
+    }
+
+    num_repos = ...  # type: int
+    num_pyfiles = ...  # type: int
+    num_reqfiles = ...  # type: int
+
+    repos = ...  # type: Set[str]
+    reqfiles = ...  # type: Dict[str, str]
+    pyfiles = ...  # type: DefaultDict[str, Set[str]]
+
+    @classmethod
+    def use_val(cls, value, field):
+        value = super(Package, cls).use_val(value, field)
+        if field == 'repos':
+            return set(value)
+        elif field == 'pyfiles':
+            return defaultdict(set, {k: set(v) for k, v in value.items()})
+
+    @classmethod
+    def store_val(cls, value, field):
+        if field == 'pyfiles':
+            value = {k: list(v) for k, v in value.items()}
+        return super(Package, cls).store_val(value, field)
+
+    def add_repo(self, repo):
+        if repo not in self.repos:
+            self.repos.add(repo)
+            self.num_repos += 1
+            self.set_change('repos', 'num_repos')
+
+    def add_pyfile(self, path, repo):
+        self.add_repo(repo)
+        if path not in self.pyfiles[repo]:
+            self.pyfiles[repo].add(path)
+            self.num_pyfiles += 1
+            self.set_change('pyfiles', 'num_pyfiles')
+
+    def add_pkgver(self, version, repo):
+        self.add_repo(repo)
+        if repo not in self.reqfiles:
+            self.reqfiles[repo] = version
+            self.num_reqfiles += 1
+            self.set_change('reqfiles', 'num_reqfiles')
+        else:
+            self.reqfiles[repo] = version
+            self.set_change('reqfiles')
+
+    def has_reqfile(self, repo):
+        return repo in self.reqfiles
+
+    def get_pkgver(self, repo):
+        return self.reqfiles.get(repo)
 
 
 class Repository(HashType):
     _prefix = 'repo'
     _text_fields = ('id', 'url')
-    _json_fields = ('pyfiles', 'reqfile')
+    _json_fields = ('files', 'packages')
 
-    reqfile = ...  # type: Dict[Any, Any]
-    pyfiles = ...  # type: Dict[Any, Any]
+    _defaults = {
+        'files': RepoFiles(),
+        'packages': []
+    }
 
-    def add_pyfile(self, path, content):
-        if _is_notset(self.pyfiles):
-            self.pyfiles = {}
-        self.pyfiles[path] = content
+    id = ...  # type: str
+    url = ...  # type: str
 
-    def set_reqfile(self, path, content):
-        self.reqfile = {path: content}
+    files = ...  # type: RepoFiles
+    packages = ...  # type: List[str]
+
+    @classmethod
+    def use_val(cls, value, field):
+        value = super(Repository, cls).use_val(value, field)
+        if field == 'files':
+            return RepoFiles(files=value)
+        else:
+            return value
+
+    @classmethod
+    def store_val(cls, value, field):
+        if field == 'files':
+            value = value.as_dict()
+        return super(Repository, cls).store_val(value, field)
+
+    @staticmethod
+    def expects(path):
+        return RepoFiles.is_pyfile(path) or RepoFiles.is_reqfile(path)
+
+    def exists(self, path):
+        return path in self.files
+
+    def get_content(self, path):
+        try:
+            return self.files[path]
+        except KeyError:
+            return None
+
+    def set_id(self, id_):
+        self.id = id_
+        self.set_change('id')
+
+    def set_url(self, url):
+        self.url = url
+        self.set_change('url')
+
+    def add_file(self, path, content):
+        try:
+            self.files[path] = content
+        except KeyError:
+            return False
+        else:
+            self.set_change('files')
+            return True
+
+    def find_packages(self):
+        ext_packages = {}
+        local_packages = self.files.local_packages()
+
+        # Find packages in python modules
+        for path, content in self.files.pyfiles:
+            packages = RepoFiles.parse_pyfile(content)
+            for pkgname in packages - local_packages:
+                pkg = ext_packages.get(pkgname) or Package(pkgname)
+                pkg.add_pyfile(path, self.name)
+                if pkgname not in ext_packages:
+                    ext_packages[pkgname] = pkg
+
+        # Find packages in requirement file
+        if _is_set(self.files.reqfile):
+            _, content = self.reqfile
+            packages = RepoFiles.parse_reqfile(content)
+            for pkgname, version in packages.items():
+                if pkgname not in local_packages:
+                    pkg = ext_packages.get(pkgname) or Package(pkgname)
+                    pkg.add_pkgver(version, self.name)
+                    if pkgname not in ext_packages:
+                        ext_packages[pkgname] = pkg
+
+        # Commit packages' changes
+        for pkg in ext_packages.values():
+            pkg.commit_changes()
+
+        # Store package name in repo
+        self.packages = list(ext_packages.keys())
+        self.set_change('packages')

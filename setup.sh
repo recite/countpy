6b0d1ec479be75e10002e12ed50617c8ce7a2de2
khanh <khanhtd@gmail.com>
2018-10-01 22:18:14 +0700

    Added nginx and fixes for setup script.

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ curdir=`pwd`
 appdir=$(realpath "$(dirname $0)")
 countpy_svc="/etc/systemd/system/countpy.service"
 redis_svc="/etc/systemd/system/redis.service"
+countpy_fqdn="countpy.com"
+countpy_conf="/etc/nginx/sites-available/$countpy_fqdn"
 
 # Install packages
 packages=""
@@ -21,6 +23,7 @@ dpkg --list | grep tcl &> /dev/null || packages="${packages} tcl"
 dpkg --list | grep nodejs &> /dev/null || packages="${packages} nodejs"
 dpkg --list | grep npm &> /dev/null || packages="${packages} npm"
 dpkg --list | grep git-core &> /dev/null || packages="${packages} git-core"
+dpkg --list | grep nginx &> /dev/null || packages="${packages} nginx"
 if [[ ! -z "${packages}" ]]; then
     apt-get update && apt-get install -y ${packages}
 fi
@@ -48,9 +51,12 @@ if ! which bower &> /dev/null; then
     npm install -g bower
 fi
 
+# Backward compatibility for nodejs
+[ -f /usr/bin/node ] || ln -s `which nodejs` /usr/bin/node
+
 # Install static libraries
 cd $appdir/app
-bower install
+bower install --allow-root
 cd $curdir
 
 # Install redis
@@ -64,8 +70,8 @@ if ! systemctl status redis &> /dev/null; then
     make install
     mkdir /etc/redis
     cp redis.conf /etc/redis
-    sed -i 's/^[ \t#]*supervised .+/supervised systemd/g' /etc/redis/redis.conf
-    sed -i 's:^[ \t#]*dir .+:dir /var/lib/redis:g' /etc/redis/redis.conf
+    sed -ri 's/^supervised .+/supervised systemd/g' /etc/redis/redis.conf
+    sed -ri 's:^dir .+:dir /var/lib/redis:g' /etc/redis/redis.conf
     adduser --system --group --no-create-home redis
     mkdir /var/lib/redis
     chown redis:redis /var/lib/redis
@@ -85,6 +91,16 @@ Restart=always
 WantedBy=multi-user.target
 EOF
     cd $curdir
+    rm -rf /tmp/redis-stable*
+
+    grep vm.overcommit_memory /etc/sysctl.conf > /dev/null || \
+    echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+
+    grep net.core.somaxconn /etc/sysctl.conf > /dev/null || \
+    echo "net.core.somaxconn = 65535" >> /etc/sysctl.conf
+
+    sysctl -p
+
     systemctl daemon-reload
     systemctl enable redis
     systemctl start redis
@@ -92,13 +108,17 @@ fi
 
 # Install countpy
 if ! systemctl status countpy &> /dev/null; then
+    adduser --system --group --no-create-home countpy
+    chown -R countpy.www-data $appdir
     uwsgi_cmd=`which uwsgi`
     cat > $countpy_svc <<EOF
 [Unit]
 Description=uWSGI Server for countpy app
-After=syslog.target
+After=network.target
 
 [Service]
+User=countpy
+Group=www-data
 ExecStart=$uwsgi_cmd --ini $appdir/wsgi.ini
 WorkingDirectory=$appdir
 Restart=always
@@ -115,4 +135,32 @@ EOF
     systemctl start countpy
 fi
 
+# Configure nginx
+[ -f /etc/nginx/sites-enabled/default ] && rm -f /etc/nginx/sites-enabled/default
+
+if [ ! -f $countpy_conf ]; then
+    cat > $countpy_conf <<EOF
+server {
+    listen 80;
+    server_name $countpy_fqdn www.$countpy_fqdn;
+
+    location /static {
+        alias $appdir/static;
+    }
+
+    location / {
+        include uwsgi_params;
+        uwsgi_pass unix:///$appdir/countpy.sock;
+    }
+}
+EOF
+
+    ln -s $countpy_conf /etc/nginx/sites-enabled/$countpy_fqdn
+fi
+
+# Restart nginx
+systemctl enable nginx
+systemctl restart nginx
+
+# Finished
 echo "Done!"

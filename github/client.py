0db1ed3590db4fd943d78327133c01bc7b38a153
khanh <khanhtd@gmail.com>
2018-10-20 12:33:30 +0700

    Complete ContentRetriever of Github client

--- github/client.py
+++ github/client.py
@@ -1,167 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import time
+from collections import deque
 from types import SimpleNamespace
-from requests import Session, Response, codes
-from requests.exceptions import Timeout, ConnectionError
-from json.decoder import JSONDecodeError
-from urllib.parse import splitquery, parse_qsl
-from . import _get_logger, get_endpoint, RATE_LIMIT_URL
-from .exceptions import *
+from requests import Session, codes
+from urllib.parse import splitquery, parse_qsl, urljoin
+from . import _get_logger, get_endpoint
+from .utils import parse_json
+from .limit import GithubLimit, github_limit, reconnect
 
-__all__ = ['GithubLimit', 'GithubClient', 'Pagination', 'Retriever']
+__all__ = ['GithubClient', 'Pagination', 'ContentRetriever', 'GithubContent']
 
-REQUEST_TIMEOUT = 15
-MIN_REMAINING_OF_LIMIT = 0
-MIN_DELAY_PER_REQUEST = 1
-MORE_DELAY_IF_OUT_LIMIT = .5
-SHORT_BREAK_DELAY = 1
-MEDIUM_BREAK_DELAY = 3
-LONG_BREAK_DELAY = 5
-MAX_RETRIES_PER_REQUEST = 5
+DEFAULT_REQUEST_TIMEOUT = 15
 MAX_RESULTS_PER_PAGE = 100
 
 
-def retry(task):
-    def waiter(client, *args, **kwargs):
-        client.limit.ask(session=client.session)
-        client.limit.delay()
-
-        count = 0
-        while count < MAX_RETRIES_PER_REQUEST:
-            try:
-                result = task(client, *args, **kwargs)
-            except RateLimitError as exc:
-                client.logger.error('GitHub limit exceeded: [%s]' % exc)
-                client.limit.ask(session=client.session, force=True)
-                client.delay(SHORT_BREAK_DELAY)
-            except AbuseLimitError as exc:
-                client.logger.error('GitHub abuse limit violated: [%s]' % exc)
-                client.reset()
-                client.delay(LONG_BREAK_DELAY)
-            except GithubException as exc:
-                client.logger.error('GitHub responds error: [%s]' % exc)
-                if not isinstance(exc, (DataDecodeError, NotFoundError)):
-                    raise
-                client.delay(MEDIUM_BREAK_DELAY)
-            except (Timeout, ConnectionError) as exc:
-                client.logger.error(
-                    'Request timeout or connection error: [%s]' % exc)
-                client.reset()
-                client.delay(LONG_BREAK_DELAY)
-            else:
-                client.limit.use()
-                return result
-
-            count += 1
-            client.logger.info('Retrying the request...')
-
-    return waiter
-
-
-def parse_response(response):
-    assert isinstance(response, Response), 'Invalid response object.'
-    try:
-        data = response.json()
-    except JSONDecodeError:
-        cls = DataDecodeError
-        data = response.text
-    else:
-        if response.status_code is codes.OK:
-            return data, response
-
-        cls = GithubException
-        message = data.get('message', '').lower()
-
-        if response.status_code is codes.UNAUTHORIZED:
-            cls = LoginError
-
-        elif response.status_code is codes.FORBIDDEN:
-            if 'invalid user-agent' in message:
-                cls = UserAgentError
-            elif 'rate limit exceeded' in message:
-                cls = RateLimitError
-            elif 'abuse' in message:
-                cls = AbuseLimitError
-
-        elif response.status_code is codes.NOT_FOUND:
-            cls = NotFoundError
-
-    raise cls(response.status_code, data)
-
-
-class GithubLimit:
-    def __init__(self, endpoint):
-        self.limit = 0
-        self.remaining = 0
-        self.reset = 0
-        self._last_use = 0
-        self._delay = None
-        self._key = 'search' if endpoint.startswith('search') else 'core'
-
-    @property
-    def now(self):
-        return time.time()
-
-    def has_data(self):
-        return bool(self.reset) is True
-
-    def _set_delay(self):
-        assert self.has_data(), 'No limit data found.'
-        delay = (self.reset - self.now) / self.limit
-        if delay < MIN_DELAY_PER_REQUEST:
-            self._delay = MIN_DELAY_PER_REQUEST
-        else:
-            self._delay = delay
-
-    def ask(self, session, force=False):
-        if force or self.stale():
-            response = session.get(RATE_LIMIT_URL)
-            data, _ = parse_response(response)
-            self.update(data['resources'][self._key])
-            self._set_delay()
-
-    def update(self, data):
-        for key, val in data.items():
-            if hasattr(self, key):
-                setattr(self, key, val)
-
-    def stale(self):
-        return self.reset <= self.now
-
-    def in_limit(self):
-        return self.remaining > MIN_REMAINING_OF_LIMIT
-
-    def delay(self):
-        # Calculate delay value
-        delay = 0
-        if self.in_limit():
-            if self._last_use and self._delay:
-                delay = self._last_use + self._delay - self.now
-        else:
-            delay = self.reset - self.now + MORE_DELAY_IF_OUT_LIMIT
-
-        # Do delay
-        if delay > 0:
-            time.sleep(delay)
-
-    def use(self):
-        self.remaining -= 1
-        self._last_use = self.now
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return '{}(limit={}, remaining={}, reset={})'.format(
-            self.__class__.__name__, self.limit, self.remaining, self.reset)
-
-
 class GithubClient:
+    default_headers = {
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
     def __init__(self, endpoint=None, auth=None, headers=None, timeout=None):
         self.method, self.url = get_endpoint(endpoint)
-        self.default_headers = headers or {}
-        self.timeout = timeout or REQUEST_TIMEOUT
+        if headers:
+            self.default_headers.update(headers)
+        self.timeout = timeout or DEFAULT_REQUEST_TIMEOUT
         self.auth = auth
         self.limit = GithubLimit(endpoint)
         self.logger = _get_logger(self.__class__.__name__)
@@ -182,7 +45,7 @@ class GithubClient:
                          % (seconds, 's' if seconds > 1 else ''))
         time.sleep(seconds)
 
-    @retry
+    @github_limit
     def request(self, method=None, url=None, data=None, **kwargs):
         method = method or self.method
         url = url or self.url
@@ -195,7 +58,7 @@ class GithubClient:
         response = self.session.request(
             method, url, timeout=timeout, json=data, **kwargs)
 
-        return parse_response(response)
+        return parse_json(response)
 
 
 class Pagination(GithubClient):
@@ -248,7 +111,7 @@ class Pagination(GithubClient):
         assert self.has_next()
         self.request(url=self._links['next'])
 
-    def iter(self):
+    def traverse(self):
         if self.has_data():
             while True:
                 for i in self.items:
@@ -259,5 +122,47 @@ class Pagination(GithubClient):
                 break
 
 
-class Retriever(GithubClient):
-    pass
+class GithubContent(SimpleNamespace):
+    def is_file(self):
+        return self.type == 'file'
+
+    @reconnect
+    def __download(self):
+        if self.download_url is not None:
+            with Session() as s:
+                resp = s.get(self.download_url)
+            assert resp.status_code is codes.OK, resp
+            return resp.text
+
+    @property
+    def content(self):
+        assert self.is_file(), 'Not a file.'
+        if not hasattr(self, '_content'):
+            self._content = self.__download()
+        return self._content
+
+
+class ContentRetriever(GithubClient):
+    def __init__(self, contents_url, auth=None, timeout=None):
+        super(ContentRetriever, self).__init__(auth=auth, timeout=timeout)
+        self.method, self.url = 'GET', contents_url
+
+    def retrieve(self, path='.'):
+        data, _ = self.request(url=urljoin(self.url, path))
+        if not isinstance(data, list):
+            data = [data]
+        yield from (GithubContent(**i) for i in data)
+
+    def traverse(self):
+        folders = deque(['.'])
+        while True:
+            try:
+                folder = folders.popleft()
+            except IndexError:
+                break
+            else:
+                for item in self.retrieve(folder):
+                    if item.is_file():
+                        yield item
+                    else:
+                        folders.append(item.path)

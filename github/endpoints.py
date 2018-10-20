5a6dd5bc61b7d805e2302d662d831bbfb4798193
khanh <khanhtd@gmail.com>
2018-10-20 20:05:04 +0700

    Build SearchRepositories endpoint

--- github/endpoints.py
+++ github/endpoints.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+from .client import GithubSearch
+
+
+class SearchRepositories(GithubSearch):
+    def __init__(self, **kwargs):
+        super(SearchRepositories, self).__init__(
+            endpoint='search_repositories', **kwargs)

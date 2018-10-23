1f1d4b5923a1e201cdb7cd328063c5e9e61dd500
khanh <khanhtd@gmail.com>
2018-10-24 00:07:25 +0700

    Revised the handling for Github errors.

--- lib/github/exceptions.py
+++ lib/github/exceptions.py
@@ -6,8 +6,11 @@ github.exceptions
 
 This module contains the set of Github's exceptions.
 """
+import time
 from requests import Response, codes
+from requests.exceptions import Timeout, ConnectionError
 from json.decoder import JSONDecodeError
+from . import _get_logger
 
 __all__ = [
     'GithubException',
@@ -18,8 +21,91 @@ __all__ = [
     'RateLimitError',
     'AbuseLimitError',
     'MaxRetriesExceeded',
-    'parse_response'
+    'parse_response',
+    'handle_exception'
 ]
+
+SHORT_BREAK_DELAY = 2
+MEDIUM_BREAK_DELAY = 5
+LONG_BREAK_DELAY = 10
+DEFAULT_BREAK_DELAY = LONG_BREAK_DELAY
+
+
+class GithubException(Exception):
+    delay = None
+    prefix = 'GitHub responds error: '
+
+    def __init__(self, status, data):
+        super(GithubException, self).__init__()
+        self.status = status
+        self.data = data
+
+    def __str__(self):
+        return '%s%s %s' % (self.prefix, self.status, self.data)
+
+
+class DataDecodeError(Exception):
+    delay = MEDIUM_BREAK_DELAY
+
+
+class NotFoundError(GithubException):
+    delay = MEDIUM_BREAK_DELAY
+
+
+class LoginError(GithubException):
+    pass
+
+
+class UserAgentError(GithubException):
+    pass
+
+
+class RateLimitError(GithubException):
+    delay = SHORT_BREAK_DELAY
+    prefix = 'GitHub limit exceeded: '
+
+
+class AbuseLimitError(GithubException):
+    delay = LONG_BREAK_DELAY
+    prefix = 'GitHub abuse limit violated: '
+
+
+class GithubServerError(GithubException):
+    delay = LONG_BREAK_DELAY
+
+
+class MaxRetriesExceeded(Exception):
+    pass
+
+
+_logger = _get_logger('exceptions')
+_need_reask_errors = (RateLimitError,)
+_need_reset_errors = (Timeout, ConnectionError, AbuseLimitError)
+
+
+def handle_exception(exception, delay_multiple=1, client=None):
+    def delay(s):
+        _logger.info('Resume in %s second%s...' % (s, 's' if s > 1 else ''))
+        time.sleep(s * delay_multiple)
+
+    _logger.error(exception)
+
+    seconds = None
+    if hasattr(exception, 'delay'):
+        seconds = exception.delay
+    elif isinstance(exception, (Timeout, ConnectionError)):
+        seconds = DEFAULT_BREAK_DELAY
+
+    if seconds is not None:
+        delay(seconds)
+        if client is not None:
+            if isinstance(exception, _need_reask_errors):
+                client.ask_limit(force=True)
+            elif isinstance(exception, _need_reset_errors):
+                client.reset()
+        return
+
+    raise exception
 
 
 def parse_response(response, json=True):
@@ -50,42 +136,7 @@ def parse_response(response, json=True):
         elif response.status_code is codes.NOT_FOUND:
             cls = NotFoundError
 
+        elif response.status_code in (codes.SERVER_ERROR, codes.BAD_GATEWAY):
+            cls = GithubServerError
+
     raise cls(response.status_code, data)
-
-
-class GithubException(Exception):
-    def __init__(self, status, data):
-        super(GithubException, self).__init__()
-        self.status = status
-        self.data = data
-
-    def __str__(self):
-        return '%s %s' % (self.status, self.data)
-
-
-class DataDecodeError(Exception):
-    pass
-
-
-class LoginError(GithubException):
-    pass
-
-
-class NotFoundError(GithubException):
-    pass
-
-
-class UserAgentError(GithubException):
-    pass
-
-
-class RateLimitError(GithubException):
-    pass
-
-
-class AbuseLimitError(GithubException):
-    pass
-
-
-class MaxRetriesExceeded(Exception):
-    pass
